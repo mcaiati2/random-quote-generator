520a9a87998a1f396036cdf1040e29ef2bcfa175
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-10-18 11:03:18 -0400

    ready for deploy

--- server/dist/routes/htmlRoutes.js
+++ server/dist/routes/htmlRoutes.js
@@ -1,10 +1,13 @@
 import { Router } from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import { dirname } from 'path';
 const router = Router();
-router.get('*', (_, responseObj) => {
-    responseObj.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
-});
+if (true || process.env.PORT) {
+    router.get('*', (_, responseObj) => {
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = dirname(__filename);
+        responseObj.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+    });
+}
 export default router;
--- server/src/routes/htmlRoutes.ts
+++ server/src/routes/htmlRoutes.ts
@@ -1,14 +1,13 @@
-import {Router, Response} from 'express';
+import { Router, Response } from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+import { dirname } from 'path';
 const router = Router();
-
-router.get('*', (_, responseObj: Response) => {
-    responseObj.sendFile(path.join(__dirname, '../../../client/dist/index.html'))
-});
-
+if (true || process.env.PORT) {
+    router.get('*', (_, responseObj: Response) => {
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = dirname(__filename);
+        responseObj.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+    });
+}
 export default router;
